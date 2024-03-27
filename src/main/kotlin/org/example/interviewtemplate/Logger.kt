245b0570cfac64ed42d453e9d025a2d081e5fe97
George Papadopoulos <george.719pap@gmail.com>
2024-03-27 19:34:58 +0200

    Add Logger.kt

---
+++ src/main/kotlin/org/example/interviewtemplate/Logger.kt
@@ -0,0 +1,26 @@
+package org.example.interviewtemplate
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
+
+// When needing to instantiate a logger for a class, do this:
+// private val logger = logger()
+inline fun <reified T> T.logger(): Logger =
+    if (T::class.isCompanion)
+        LoggerFactory.getLogger(T::class.java.enclosingClass)
+    else
+        LoggerFactory.getLogger(T::class.java)
+
+// To write to metric log do this:
+// logger.metric("message")
+fun Logger.metric(msg: String) {
+    this.info(MarkerFactory.getMarker("METRIC"), msg)
+}
+
+// -------------------------------- lazy-factories --------------------------------
+
+//@JvmName("Debug0")
+fun Logger.debug(message: () -> String) {
+    if (isDebugEnabled) debug(message.toString())
+}
