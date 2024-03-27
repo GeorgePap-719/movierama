df55de9ccba895d41511d7e19b1b934e4b0e4a69
George Papadopoulos <george.719pap@gmail.com>
2024-03-27 19:57:18 +0200

    Add UserOrm.kt

---
+++ src/main/kotlin/org/example/interviewtemplate/UserOrm.kt
@@ -0,0 +1,49 @@
+package org.example.interviewtemplate
+
+import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitSingle
+import kotlin.contracts.ExperimentalContracts
+
+suspend fun mapToUserEntity(spec: DatabaseClient.GenericExecuteSpec): UserEntity? {
+    return buildUser {
+        val fetchSpec = spec.map { row, metadata ->
+            check(metadata.columnMetadatas.size == 4) {
+                "Size should be equal of `UserEntity` fields."
+            }
+            id = row.getColumn<Int>("id")
+            name = row.getColumn<String>("name")
+            lastName = row.getColumn<String>("last_name")
+            phone = row.getColumn<String>("phone")
+        }
+        fetchSpec.awaitSingle()
+    }
+}
+
+private inline fun buildUser(action: UserBuilder.() -> Unit): UserEntity? {
+    val builder = UserBuilder()
+    builder.action()
+    return builder.build()
+}
+
+private class UserBuilder {
+    var id: Int? = null
+    var name: String? = null
+    var lastName: String? = null
+    var phone: String? = null
+
+    val isEmpty: Boolean get() = id == null && name == null && lastName == null && phone == null
+
+    fun build(): UserEntity? {
+        if (isEmpty) return null
+        val id = requireNotNull(id)
+        val name = requireNotNull(name)
+        val lastName = requireNotNull(lastName)
+        val phone = requireNotNull(phone)
+        return UserEntity(
+            id = id,
+            name = name,
+            lastName = lastName,
+            phone = phone
+        )
+    }
+}
